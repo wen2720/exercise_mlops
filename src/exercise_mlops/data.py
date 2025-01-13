c2f3aad923ac238f9dea1974049376d4e34172bc
wen2720 <wenhao2720@gmail.com>
2025-01-13 22:25:30 +0100

    m6 exercise

--- src/exercise_mlops/data.py
+++ src/exercise_mlops/data.py
@@ -1,29 +1,49 @@
-from pathlib import Path
-
+import torch
 import typer
-from torch.utils.data import Dataset
 
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+def normalize(images: torch.Tensor) -> torch.Tensor:
+    """Normalize images."""
+    return (images - images.mean()) / images.std()
 
-    def __init__(self, raw_data_path: Path) -> None:
-        self.data_path = raw_data_path
 
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
+def preprocess_data(raw_dir: str, processed_dir: str) -> None:
+    """Process raw data and save it to processed directory."""
+    train_images, train_target = [], []
+    for i in range(6):
+        train_images.append(torch.load(f"{raw_dir}/train_images_{i}.pt"))
+        train_target.append(torch.load(f"{raw_dir}/train_target_{i}.pt"))
+    train_images = torch.cat(train_images)
+    train_target = torch.cat(train_target)
 
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
+    test_images: torch.Tensor = torch.load(f"{raw_dir}/test_images.pt")
+    test_target: torch.Tensor = torch.load(f"{raw_dir}/test_target.pt")
 
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
+    train_images = train_images.unsqueeze(1).float()
+    test_images = test_images.unsqueeze(1).float()
+    train_target = train_target.long()
+    test_target = test_target.long()
 
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    dataset = MyDataset(raw_data_path)
-    dataset.preprocess(output_folder)
+    train_images = normalize(train_images)
+    test_images = normalize(test_images)
+
+    torch.save(train_images, f"{processed_dir}/train_images.pt")
+    torch.save(train_target, f"{processed_dir}/train_target.pt")
+    torch.save(test_images, f"{processed_dir}/test_images.pt")
+    torch.save(test_target, f"{processed_dir}/test_target.pt")
+
+
+def corrupt_mnist() -> tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+    """Return train and test datasets for corrupt MNIST."""
+    train_images = torch.load("data/processed/train_images.pt")
+    train_target = torch.load("data/processed/train_target.pt")
+    test_images = torch.load("data/processed/test_images.pt")
+    test_target = torch.load("data/processed/test_target.pt")
+
+    train_set = torch.utils.data.TensorDataset(train_images, train_target)
+    test_set = torch.utils.data.TensorDataset(test_images, test_target)
+    return train_set, test_set
 
 
 if __name__ == "__main__":
-    typer.run(preprocess)
+    typer.run(preprocess_data)
