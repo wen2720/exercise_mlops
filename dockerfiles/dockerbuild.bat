27ed368fad31d451bceadae1d1d5b758f44250d0
wen2720 <wenhao2720@gmail.com>
2025-01-20 23:42:21 +0100

    docker gcloud connection, testing: trigger

--- dockerfiles/dockerbuild.bat
+++ dockerfiles/dockerbuild.bat
@@ -1,3 +1,8 @@
-docker build -f dockerfiles/train.dockerfile -t corruptmnist:latest .
+:: build docker image with the command defined in the file, for local development
+::docker build -f dockerfiles/train.dockerfile -t corruptmnist:latest .
+
+:: building image for GCP
+docker build -f dockerfiles/train.dockerfile -t europe-west4-docker.pkg.dev/dtup-mlop/corruptmnist:latest .
+
+:: --progress=plain option
 ::docker build --progress=plain -f dockerfiles/train.dockerfile -t corruptmnist:latest .
-::docker build -f dockerfiles/train.dockerfile -t train:latest .
