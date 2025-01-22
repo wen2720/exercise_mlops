653c8784e227d95e823131487a667a45f430a54a
wen2720 <wenhao2720@gmail.com>
2025-01-22 22:08:40 +0100

    m15 and [project.scripts]

--- src/exercise_mlops/train_profiling.py
+++ src/exercise_mlops/train_profiling.py
@@ -4,6 +4,8 @@ import typer
 import wandb
 from data import corrupt_mnist
 from model import MyAwesomeModel
+from torch.profiler import profile, tensorboard_trace_handler,  ProfilerActivity
+
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Training on {DEVICE}")
@@ -12,12 +14,6 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 5) -> None:
     """Train a model on MNIST."""
     print("Training day and night")
     print(f"{lr=}, {batch_size=}, {epochs=}")
-
-    wandb.init(
-        project="corrupt_mnist",
-        config={"lr": lr, "batch_size": batch_size, "epochs": epochs},
-    )
-
 
     model = MyAwesomeModel().to(DEVICE)
     train_set, _ = corrupt_mnist()
@@ -28,25 +24,31 @@ def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 5) -> None:
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     statistics = {"train_loss": [], "train_accuracy": []}
+    with profile(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        on_trace_ready=tensorboard_trace_handler("src/exercise_mlops/profiling/resnet18"),  # Directory for saving TensorBoard logs
+        schedule=torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=2),
+        with_stack=True,  # Collect stack trace information
+        record_shapes=True,  # Record tensor shapes
+    ) as prof:
         
-    for epoch in range(epochs):
-        model.train()
-        for i, (img, target) in enumerate(train_dataloader):
-            img, target = img.to(DEVICE), target.to(DEVICE)
-            optimizer.zero_grad()
-            y_pred = model(img)
-            loss = loss_fn(y_pred, target)
-            loss.backward()
-            optimizer.step()
-            statistics["train_loss"].append(loss.item())
+        for epoch in range(epochs):
+            model.train()
+            for i, (img, target) in enumerate(train_dataloader):
+                img, target = img.to(DEVICE), target.to(DEVICE)
+                optimizer.zero_grad()
+                y_pred = model(img)
+                loss = loss_fn(y_pred, target)
+                loss.backward()
+                optimizer.step()
+                statistics["train_loss"].append(loss.item())
 
-            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
-            statistics["train_accuracy"].append(accuracy)
+                accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+                statistics["train_accuracy"].append(accuracy)
 
-            if i % 100 == 0:
-                #print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
-                wandb.log({"Epoch": epoch, "iter": i, "loss": loss.item()})
-            
+                if i % 100 == 0:
+                    print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+                prof.step()
 
     print("Training complete")
     torch.save(model.state_dict(), "models/corruptmnist/model.pth")
